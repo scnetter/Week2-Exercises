5b4b9275d86c7678879d13b024c5a30f01b253be
Kelly <36lima@gmail.com>
2015-11-09 16:11:06 -0500

    Added BlackJack class to run game from.

--- blackjack_oob.rb
+++ blackjack_oob.rb
@@ -1,35 +1,100 @@
 require 'pry'
 
-def show_winner(hand_one, hand_two)
-  if hand_one.hand_owner == 'Dealer'
-    dealer_total = hand_one.get_total
-    player_total = hand_two.get_total
-  else 
-    dealer_total = hand_two.get_total
-    player_total = hand_one.get_total
+class BlackJack
+  attr_accessor :deck, :player_hand, :dealer_hand
+
+  def initialize(number_of_decks,player_name)
+    @deck = Deck.new(number_of_decks)
+    @player_hand = CardHand.new(player_name)
+    @dealer_hand = CardHand.new("Dealer")
   end
 
-  case 
-  when player_total > 21
-    puts "Dealer Wins - Player Bust"
-    return
-  when dealer_total > 21
-    puts "Player Wins! - Dealer Bust"
-    return
-  when dealer_total == player_total
-    puts "Dealer wins all ties - House Rules!!"
-    return  
-  when dealer_total == 21
-    puts "Dealer Wins! - Blackjack!"
-    return
-  when player_total == 21
-    puts "Player Wins! - Blackjack!"
-    return
-  when dealer_total > player_total
-    puts "Dealer Wins with #{dealer_total}. Player with #{player_total}"
-    return
-  else
-    puts "Player wins with #{player_total}. Dealer with #{dealer_total}"
+  def show_winner(hand_one, hand_two)
+    if hand_one.hand_owner == 'Dealer'
+      dealer_total = hand_one.get_total
+      player_total = hand_two.get_total
+    else 
+      dealer_total = hand_two.get_total
+      player_total = hand_one.get_total
+    end
+
+    case 
+    when player_total > 21
+      puts "Dealer Wins - Player Bust"
+      return
+    when dealer_total > 21
+      puts "Player Wins! - Dealer Bust"
+      return
+    when dealer_total == player_total
+      puts "Dealer wins all ties - House Rules!!"
+      return  
+    when dealer_total == 21
+      puts "Dealer Wins! - Blackjack!"
+      return
+    when player_total == 21
+      puts "Player Wins! - Blackjack!"
+      return
+    when dealer_total > player_total
+      puts "Dealer Wins with #{dealer_total}. Player with #{player_total}"
+      return
+    else
+      puts "Player wins with #{player_total}. Dealer with #{dealer_total}"
+    end
+  end
+
+  def renew_deck
+    self.deck = Deck.new(number_of_decks)
+  end
+
+  def clear_hands
+    self.player_hand.cards = []
+    self.dealer_hand.cards = []
+  end
+
+  def run_game
+    begin
+      if deck.cards.length < (deck.number_of_decks.to_i * 52) / 2 
+        print "Generating new deck. . ."
+        self.deck = Deck.new(number_of_decks)
+      end
+
+      2.times do
+        self.player_hand.cards << deck.deal
+        self.dealer_hand.cards << deck.deal
+      end
+
+      player_hand.show_hand
+      dealer_hand.show_hand
+
+      loop do
+        break if player_hand.get_total == 21
+        puts "What would you like to do? 1) Hit 2) Stay:"
+        hit_or_stay = gets.chomp
+        if !['1','2'].include?(hit_or_stay)
+          puts 'You must enter 1) to Hit or 2) to Stay'
+          next
+        elsif hit_or_stay == '1'
+          player_hand.cards << deck.deal
+          player_hand.show_hand
+          player_hand.get_total >= 21 ? break : next
+        else
+          player_hand.show_hand
+          break
+        end
+      end
+
+      dealer_hand.show_hand
+        
+      until dealer_hand.get_total >= 17 || player_hand.get_total > 21 || 
+        (player_hand.get_total == 21 && dealer_hand.get_total >= 17)
+        puts "Dealer draws. . ."
+        self.dealer_hand.cards << deck.deal
+        dealer_hand.show_hand
+      end
+      show_winner(dealer_hand,player_hand)
+      clear_hands
+      puts "Do you want to play again? [Y/N]:"
+    end until gets.chomp.downcase != 'y'
   end
 end
 
@@ -74,19 +139,22 @@ end
 
 
 class Deck
-  attr_accessor :cards
+  attr_accessor :cards, :number_of_decks
 
-  def initialize
+  def initialize(number_of_decks=1)
+    @number_of_decks = number_of_decks
     @cards = []
     card_values = %w[H D S C].product(%w[2 3 4 5 6 7 8 9 10 J Q K A])
     card_values.each do |suit_value_array|
-      @cards << Card.new(suit_value_array[0],suit_value_array[1])
+      number_of_decks.to_i.times do
+        @cards << Card.new(suit_value_array[0],suit_value_array[1])
+      end
     end
     @cards.shuffle!
   end
 
-  def deal(card_hand)
-    card_hand.cards << self.cards.pop 
+  def deal
+    cards.pop 
   end
 end
 
@@ -106,52 +174,10 @@ end
 puts "-----------Let's play BlackJack!--------------"
 puts "What's your name?"
 player_name = gets.chomp
-
-begin
-
-  deck = Deck.new
-  player_hand = CardHand.new(player_name)
-  dealer_hand = CardHand.new("Dealer")
-
-  2.times do
-    deck.deal(player_hand)
-    deck.deal(dealer_hand)
-  end
-  player_hand.show_hand
-  dealer_hand.show_hand
-
-  loop do
-    break if player_hand.get_total == 21
-    puts "What would you like to do? 1) Hit 2) Stay:"
-
-    hit_or_stay = gets.chomp
-    if !['1','2'].include?(hit_or_stay)
-      puts 'You must enter 1) to Hit or 2) to Stay'
-      next
-    elsif hit_or_stay == '1'
-      deck.deal(player_hand)
-      player_hand.show_hand
-      player_hand.get_total >= 21 ? break : next
-    else
-      player_hand.show_hand
-      break
-    end
-  end
-
-
-  dealer_hand.show_hand
-    
-    until dealer_hand.get_total >= 17 || player_hand.get_total > 21 
-      || (player_hand.get_total == 21 && dealer_hand.get_total >= 17)
-      puts "Dealer draws. . ."
-      deck.deal(dealer_hand)
-      dealer_hand.show_hand
-    end
-  end
-  show_winner(dealer_hand,player_hand)
-  puts "Do you want to play again? [Y/N]:"
-
-end until gets.chomp.downcase != 'y'
+puts "How many decks do you want to play with?"
+number_of_decks = gets.chomp
+current_game = BlackJack.new(number_of_decks,player_name)
+current_game.run_game
 
 
 
